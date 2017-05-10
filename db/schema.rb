cc15bfec08ddc77755fa522d20fb935bbfbb45ab
RobertRuttenberg <rdruttenberg@gmail.com>
2017-05-10 17:21:02 -0400

    migrate add url

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509202841) do
+ActiveRecord::Schema.define(version: 20170510190928) do
 
   create_table "art_pieces", force: :cascade do |t|
     t.string "title"
     t.string "artist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
 end
