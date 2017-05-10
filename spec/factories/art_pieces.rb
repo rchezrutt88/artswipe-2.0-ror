5fa3c7cd3f58bf543deb6705c53ac65d12c420ce
RobertRuttenberg <rdruttenberg@gmail.com>
2017-05-10 18:03:26 -0400

    art_piece factory now uses faker

--- spec/factories/art_pieces.rb
+++ spec/factories/art_pieces.rb
@@ -1,7 +1,31 @@
 FactoryGirl.define do
+
   factory :art_piece do
-    title 'The Girl with a Pearl Earing'
-    artist 'Johannes Vermeer'
-    url 'https://s3.amazonaws.com/artswipe/art-images/Girl_With_A_Pearl_Earring.jpg'
+
+    title { "Portrait of #{Faker::Ancient.hero}" }
+    artist { Faker::Name.name }
+    url { Faker::Avatar.image }
+
+
+    trait :void do
+      title nil
+      artist nil
+      url nil
+    end
+
+    factory :void_art_piece, traits: [:void]
+
+    factory :girl_with_pearl_earring do
+      title 'The Girl with a Pearl Earring'
+      artist 'Johannes Vermeer'
+      url 'https://s3.amazonaws.com/artswipe/art-images/Girl_With_A_Pearl_Earring.jpg'
+    end
+
+    factory :portrait_of_the_journalist_sylvia_von_harden do
+      title 'Portrait of the Journalist Sylvia von Harden'
+      artist 'Otto Dix'
+      url 'https://s3.amazonaws.com/artswipe/art-images/Sylvia-von-Harden-Otto-Dix-831x1024.jpg'
+    end
+
   end
 end
