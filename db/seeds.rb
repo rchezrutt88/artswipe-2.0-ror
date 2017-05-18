d63c4f2e3fa64bbb9fa6e80efa9d57a162560771
RobertRuttenberg <rdruttenberg@gmail.com>
2017-05-18 11:32:34 -0400

    start adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# TODO use factories to add some data here
+
+ArtPiece.create(
+    title: 'The Girl with a Pearl Earring',
+    artist: 'Johannes Vermeer',
+    url: 'https://s3.amazonaws.com/artswipe/art-images/Girl_With_A_Pearl_Earring.jpg'
+)
+
+ArtPiece.create(
+    title: 'Portrait of the Journalist Sylvia von Harden',
+    artist: 'Otto Dix',
+    url: 'https://s3.amazonaws.com/artswipe/art-images/Sylvia-von-Harden-Otto-Dix-831x1024.jpg'
+)
+
