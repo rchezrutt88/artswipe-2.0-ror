63809d62e898085604f27aee4029ae9f721f851c
Robert Ruttenberg <rdruttenberg@gmail.com>
2017-09-20 13:33:10 -0400

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'art_works#index'
+  root 'base#show'
 
   resources :art_works
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
