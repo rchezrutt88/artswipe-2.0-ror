64fd81d2af286ed4e2a1ab7570a88122f2d831a1
Robert Ruttenberg <rdruttenberg@gmail.com>
2017-09-26 16:16:04 -0400

    upvote/downvote jquery updates page

--- app/helpers/artworks_helper.rb
+++ app/helpers/artworks_helper.rb
@@ -1,20 +1,29 @@
 module ArtworksHelper
+
   def thumbs_up(artwork)
-    link_to(
-      thumbs_up_icon_style(artwork),
-      upvote_artwork_url(artwork),
-      method: :put,
-      remote: true
-    )
+    if user_signed_in?
+      link_to(
+        thumbs_up_icon_style(artwork),
+        upvote_artwork_url(artwork),
+        method: :put,
+        remote: true
+      )
+    else
+      thumbs_up_icon_style artwork
+    end
   end
 
   def thumbs_down(artwork)
-    link_to(
-      thumbs_down_icon_style(artwork),
-      downvote_artwork_url(artwork),
-      method: :put,
-      remote: true
-    )
+    if user_signed_in?
+      link_to(
+        thumbs_down_icon_style(artwork),
+        downvote_artwork_url(artwork),
+        method: :put,
+        remote: true
+      )
+    else
+      thumbs_down_icon_style artwork
+    end
   end
 
   def magnific_img_tag(artwork)
@@ -31,18 +40,18 @@ module ArtworksHelper
   private
 
   def thumbs_up_icon_style(artwork)
-    if current_user.voted_up_on? artwork
-      icon('thumbs-up')
+    if user_signed_in? && current_user.voted_up_on?(artwork)
+      icon('thumbs-up', id: "thumbs-up-icon-#{artwork.id}")
     else
-      icon('thumbs-o-up')
+      icon('thumbs-o-up', id: "thumbs-up-icon-#{artwork.id}")
     end
   end
 
   def thumbs_down_icon_style(artwork)
-    if current_user.voted_down_on? artwork
-      icon('thumbs-down')
+    if user_signed_in? && current_user.voted_down_on?(artwork)
+      icon('thumbs-down', id: "thumbs-down-icon-#{artwork.id}")
     else
-      icon('thumbs-o-down')
+      icon('thumbs-o-down', id: "thumbs-down-icon-#{artwork.id}")
     end
   end
 end
