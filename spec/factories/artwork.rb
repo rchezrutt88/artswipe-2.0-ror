999dcb3840d49074543c0045f99744dd5ad7fe2e
Robert Ruttenberg <rdruttenberg@gmail.com>
2017-11-27 14:20:11 -0500

    factorybot now attaches dummy image

--- spec/factories/artwork.rb
+++ spec/factories/artwork.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     artist { Faker::Book.author }
     date_painted { Faker::Number.between(500, 2000) }
     location { Faker::Address.city }
+    art_image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'artworks', '600x600.jpg'), 'image/jpeg')}
   end
 end
